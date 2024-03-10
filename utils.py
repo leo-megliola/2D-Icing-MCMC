066402125b30b4f6cfd20c70db922531e0c24cfd
zachary-sherman <zsherman@ucsd.edu>
2024-03-10 13:54:12 -0700

    fixed partA

--- utils.py
+++ utils.py
@@ -145,7 +145,7 @@ def plot_m(m_values, burn_in=BURNIN, temp=T, savefig = None):
     print(f"magnetization mean = {m_mean}")
     print(f"magnetization std = {m_std}")
 
-def stats(temp=T, steps=STEPS, burn_in=BURNIN, size=N):
+def stats(temp, steps=STEPS, burn_in=BURNIN, size=N):
 
     m_values, _, __ = spins(steps, random=False, temp=T, size=N)
     return np.mean(m_values[burn_in:]), np.std(m_values[burn_in:])
