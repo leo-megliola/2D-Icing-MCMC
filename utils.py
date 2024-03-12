a6e12a3d8afe7dd499781ad4ba97d932b796c728
DimaPdemler <Dimademler@gmail.com>
2024-03-11 19:39:11 -0700

    added gif making stuff

--- utils.py
+++ utils.py
@@ -95,27 +95,35 @@ def spins(steps=STEPS, random=True, temp=T, size=N, J=J, KB=KB, B=B, display=Tru
 
 
 
-def plot_lattice(lattice_spins, N=N, savefig = None):
+def plot_lattice(lattice_spins, N=N, savefig=None, show_plot=True):
     """
-    Plot the lattice spins.
+    Plot the lattice spins and optionally save the figure with a transparent background.
 
     Parameters:
-    lattice_spins (numpy.ndarray): The array representing the lattice spins.
-    N (int): The size of the lattice.
-
-    Returns:
-    None
+    - lattice_spins (numpy.ndarray): The array representing the lattice spins.
+    - N (int): The size of the lattice.
+    - savefig (str): Path to save the figure.
+    - show_plot (bool): Whether to show the plot.
     """
+    if not os.path.exists('figs') and savefig is not None:
+        os.makedirs('figs')
 
     plt.figure()
     plt.imshow(lattice_spins, cmap="RdYlBu")
-    # show gridlines
     for i in range(N):
         plt.axhline(i + 0.5, color="black", lw=0.1)
         plt.axvline(i + 0.5, color="black", lw=0.1)
-    plt.show()
+    
+    plt.axis('off')  # This will remove the axes
+    
+    if show_plot:
+        plt.show()
+
     if savefig is not None:
-        plt.savefig(os.path.join('figs', savefig))   
+        # Save the figure with a transparent background
+        plt.savefig(os.path.join('figs', savefig), transparent=True)
+        plt.close()
+
 
 
 
@@ -159,3 +167,75 @@ def stats(temp=T, steps=STEPS, burn_in=BURNIN, size=N, B=B, display=True):
 
     m_values, _, __ = spins(steps, random=False, temp=temp, size=size, B=B, display=display)
     return np.mean(m_values[burn_in:]), np.std(m_values[burn_in:])
+
+import imageio
+
+def spins_with_snapshots(steps=STEPS, random=True, temp=T, size=N, J=J, KB=KB, B=B, display=True, snapshot_interval=1000):
+    """
+    This function is an adaptation of the spins function, which also captures snapshots
+    of the lattice at each step for later visualization in a GIF.
+
+    Parameters are similar to the spins function with an added snapshot_interval parameter
+    that determines how frequently to capture the lattice state.
+    
+    Returns a list of lattice configurations at each step.
+    """
+    lattice_configurations = []
+    lattice_spins = random_spins(size) if random else np.ones((size, size))
+
+    total = np.sum(lattice_spins)
+    m_values = np.zeros((steps))
+
+    num_accept = 0
+    for t in tqdm(range(steps), disable=not display):
+        i, j = np.random.randint(size), np.random.randint(size)
+        delta_energy = 0
+        for k, l in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+            i_neigh = i + k if i + k < N else 0
+            j_neigh = j + l if j + l < N else 0
+            delta_energy += -J * -2 * lattice_spins[i, j] * lattice_spins[i_neigh, j_neigh]
+        delta_energy += -B * lattice_spins[i, j]
+        if delta_energy <= 0:
+            total -= lattice_spins[i, j]
+            lattice_spins[i, j] *= -1
+            total += lattice_spins[i, j]
+            num_accept += 1
+        else:
+            prob = np.exp(-delta_energy / (KB * temp))
+            if np.random.random() < prob:
+                total -= lattice_spins[i, j]
+                lattice_spins[i, j] *= -1
+                total += lattice_spins[i, j]
+                num_accept += 1
+        m_values[t] = total
+        if t % snapshot_interval == 0:
+            lattice_configurations.append(np.copy(lattice_spins))
+
+    return lattice_configurations
+
+def generate_ising_gif(lattice_configurations, N=N, gif_name='ising_simulation.gif', duration=0.1):
+    """
+    Generates and saves a GIF from the lattice configurations.
+
+    Parameters:
+    - lattice_configurations (list): List of 2D arrays representing lattice states over time.
+    - N (int): The size of the lattice.
+    - gif_name (str): Name of the output GIF file.
+    - duration (float): Duration of each frame in the GIF.
+    """
+    image_filenames = []
+    for index, lattice in tqdm(enumerate(lattice_configurations), total=len(lattice_configurations), desc='Creating GIF'):
+        filename = f'temp_lattice_{index}.png'
+        actual_filename = os.path.join('figs', filename)
+        plot_lattice(lattice, N, savefig=filename, show_plot=False)
+        image_filenames.append(actual_filename)
+
+    print(image_filenames)
+    with imageio.get_writer(gif_name, mode='I', duration=duration) as writer:
+        for filename in image_filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+        
+    # Clean up temporary image files
+    for filename in image_filenames:
+        os.remove(filename)
